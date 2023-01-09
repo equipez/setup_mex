91f50410b23110694c95ebdae5e8c1c4da5e52c0
zaikunzhang <zaikunzhang@gmail.com>
2023-01-09 17:15:22 +0800

    try oneapi 2023

--- windows/install_oneapi_windows.bat
+++ windows/install_oneapi_windows.bat
@@ -1,12 +1,14 @@
 REM See https://github.com/oneapi-src/oneapi-ci
 REM https://github.com/oneapi-src/oneapi-ci/blob/master/scripts/install_windows.bat
 
-set URL=https://registrationcenter-download.intel.com/akdlm/irc_nas/18529/w_HPCKit_p_2022.1.2.116_offline.exe
+REM set URL=https://registrationcenter-download.intel.com/akdlm/irc_nas/18529/w_HPCKit_p_2022.1.2.116_offline.exe
+set URL=https://registrationcenter-download.intel.com/akdlm/irc_nas/19107/w_fortran-compiler_p_2023.0.0.25579_offline.exe
 set COMPONENTS=intel.oneapi.win.ifort-compiler
 
+cd %Temp%
 curl.exe --output webimage.exe --url %URL% --retry 5 --retry-delay 5
 start /b /wait webimage.exe -s -x -f webimage_extracted --log extract.log
-del webimage.exe
+del webimage.exe extract.log
 webimage_extracted\bootstrapper.exe -s --action install --components=%COMPONENTS% --eula=accept -p=NEED_VS2017_INTEGRATION=0 -p=NEED_VS2019_INTEGRATION=0 --log-dir=.
 
 for /f "tokens=* usebackq" %%f in (`dir /b "C:\Program Files (x86)\Intel\oneAPI\compiler\" ^| findstr /V latest ^| sort`) do @set "LATEST_VERSION=%%f"
