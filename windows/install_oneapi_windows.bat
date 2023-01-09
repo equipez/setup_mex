5513f4c4b47a1c36e1e40c435e42a623abb6536d
zaikunzhang <zaikunzhang@gmail.com>
2023-01-09 17:26:27 +0800

    try latest matlab

--- windows/install_oneapi_windows.bat
+++ windows/install_oneapi_windows.bat
@@ -1,19 +1,18 @@
 REM See https://github.com/oneapi-src/oneapi-ci
 REM https://github.com/oneapi-src/oneapi-ci/blob/master/scripts/install_windows.bat
 
-REM set URL=https://registrationcenter-download.intel.com/akdlm/irc_nas/18529/w_HPCKit_p_2022.1.2.116_offline.exe
 set URL=https://registrationcenter-download.intel.com/akdlm/irc_nas/19107/w_fortran-compiler_p_2023.0.0.25579_offline.exe
 set COMPONENTS=intel.oneapi.win.ifort-compiler
 
 cd %Temp%
 curl.exe --output webimage.exe --url %URL% --retry 5 --retry-delay 5
 start /b /wait webimage.exe -s -x -f webimage_extracted --log extract.log
-del webimage.exe extract.log
-webimage_extracted\bootstrapper.exe -s --action install --components=%COMPONENTS% --eula=accept -p=NEED_VS2017_INTEGRATION=0 -p=NEED_VS2019_INTEGRATION=0 --log-dir=.
+del webimage.exe
+webimage_extracted\bootstrapper.exe -s --action install --components=%COMPONENTS% --eula=accept -p=NEED_VS2017_INTEGRATION=0 -p=NEED_VS2019_INTEGRATION=0 -p=NEED_VS2022_INTEGRATION=0 --log-dir=.
 
-for /f "tokens=* usebackq" %%f in (`dir /b "C:\Program Files (x86)\Intel\oneAPI\compiler\" ^| findstr /V latest ^| sort`) do @set "LATEST_VERSION=%%f"
-@call "C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\env\vars.bat"
+for /f "tokens=* usebackq" %%f in (`dir /b "C:\Program Files (x86)\Intel\oneAPI\compiler\" ^| findstr /V latest ^| sort`) do @set "LATEST_ONEAPI_VERSION=%%f"
+@call "C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_ONEAPI_VERSION%\env\vars.bat"
 
-echo %LATEST_VERSION%
+echo The latest Intel OneAPI installed is: %LATEST_ONEAPI_VERSION%
 where ifort.exe
 where ifx.exe
